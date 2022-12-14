a548ab681097087f84eae75afa09c60347121f42
Malay <mlychndn@gmail.com>
2022-12-15 01:46:52 +0530

    natours dev code merged

---
+++ index.js
@@ -0,0 +1,10 @@
+const allKey = ['pharma', 'global', 'xyz', 'test'];
+const storedKey = ['pharma', 'global', 'xyz'];
+
+const newKey = allKey.filter((el) => !storedKey.includes(el));
+
+console.log(newKey);
+const str = 'Malay';
+const modStr = str.toLowerCase();
+
+console.log(modStr);
