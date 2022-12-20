1647c7f01e836c267a1dc271de24ba728d2c8dc7
Malay <mlychndn@gmail.com>
2022-12-21 00:00:14 +0530

    err route defined

--- app.js
+++ app.js
@@ -31,4 +31,11 @@ app.use((req, res, next) => {
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 
+app.all('*', (req, res, next) => {
+  res.status(404).json({
+    status: 'err',
+    message: 'This route is not defined',
+  });
+});
+
 module.exports = app;
